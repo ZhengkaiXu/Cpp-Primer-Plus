170746be7ae2c01cbe7bfe16af185897cd861ca5
ZhengkaiXu <695378091@qq.com>
2024-03-23 23:16:21 +0800

    20240323

---
+++ 第三章/hexoct.cpp
@@ -0,0 +1,17 @@
+//
+// Created by xuzhengkai on 2024/3/23.
+//
+// hexoct.cpp -- shows hex and octal literials
+#include <iostream>
+int main() {
+    using namespace std;
+    int chest = 42; // decimal integer literal
+    int waist = 0x42; // hexadecimal integer literal
+    int inseam = 042; // octal integer literal
+
+    cout << "Monsieur cuts a striking figure!\n";
+    cout << "chest = " << chest << " (42 in decimal)\n";
+    cout << "waist = " << waist << " (0x42 in hex)\n";
+    cout << "inseam = " << inseam << " (042 in octal)\n";
+    return 0;
+}
---
+++ 第三章/hexoct2.cpp
@@ -0,0 +1,4 @@
+//
+// Created by xuzhengkai on 2024/3/23.
+//
+// hexoct2.cpp -- display values in hex and octal
