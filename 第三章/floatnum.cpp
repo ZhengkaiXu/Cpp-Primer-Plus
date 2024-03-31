a97ebb2d92be6e0d27d399a854c9fa73aeb3520d
ZhengkaiXu <695378091@qq.com>
2024-04-01 02:10:50 +0800

    20240401

---
+++ 第三章/floatnum.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ZhengkaiXu on 2024/4/1.
+//
+// floatnum.cpp -- floating-point types
+#include <iostream>
+int main() {
+    using namespace std;
+
+    cout.setf(ios_base::fixed, ios_base::floatfield); // fixed-point
+    float tub = 10.0 / 3.0; // good
+}
