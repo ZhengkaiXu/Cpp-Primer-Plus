5563398fb089d24791bbd846f4e7a250c2183fff
ZhengkaiXu <695378091@qq.com>
2024-04-13 17:18:54 +0800

    0413

---
+++ 第五章/num_test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ZhengkaiXu on 2024/4/13.
+//
+// num_test.cpp --use numeric test in for loop
+#include <iostream>
+int main() {
+    using namespace std;
+    cout << "Enter the starting countdown value: ";
+    int limit;
+    cin >> limit;
+    int i;
+    for (i = limit;i;i--) {// quits when i is 0
+        cout << "i = " << i << "\n";
+    }
+    cout << "Done now that i = " << i << "\n";
+    return 0;
+}
