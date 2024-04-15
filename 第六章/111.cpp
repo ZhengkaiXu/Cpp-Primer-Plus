233f7b5774c666c145085785e8600f15a019b7f0
ZhengkaiXu <695378091@qq.com>
2024-04-16 01:58:18 +0800

    0416

---
+++ 第六章/111.cpp
@@ -0,0 +1,3 @@
+//
+// Created by ZhengkaiXu on 2024/4/16.
+//
---
+++ 第六章/or.cpp
@@ -0,0 +1,22 @@
+//
+// Created by ZhengkaiXu on 2024/4/16.
+//
+// or.cpp -- using the logical OR operator
+#include <iostream>
+int main() {
+    using namespace std;
+    cout << "This program may reformat your hard disk\n"
+            "and destroy all your data.\n"
+            "Do you wish to continue? <y/n> ";
+    char ch;
+    cin >> ch;
+    if (ch == 'y' || ch == 'Y') // y or Y
+        cout << "You were warned!\a\a\n";
+    else if (ch == 'n' || ch == 'N') // n or N
+        cout << "A wish choice ... bye\n";
+    else
+        cout << "That wasn't a y or n! Apparently you "
+                "can't follow\ninstructions, so "
+                "I'll trash your disk anyway.\a\a\a\n";
+    return 0;
+}
