3010ae17df803c3170235b357d0dc9beb998435a
ZhengkaiXu <695378091@qq.com>
2024-05-20 01:16:17 +0800

    0520

--- 第十章/stock20/stock20.cpp
+++ 第十章/stock20/stock20.cpp
@@ -29,4 +29,65 @@ Stock::Stock(const std::string &co, long n, double pr) {
 Stock::~Stock() { // quiet class destructor
 }
 // other methods
-void Stock::buy(long num, double price) {}
+void Stock::buy(long num, double price) {
+    if (num < 0) {
+        std::cout << "Number of shares purchased can't be negative. "
+        << "Transaction is aborted.\n";
+    } else {
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+void Stock::sell(long num, double price) {
+    using std::cout;
+    if (num < 0) {
+        cout << "Number of shares sold can't be negative. "
+        << "Transaction is aborted.\n";
+    } else if (num > shares) {
+        cout << "You can't sell more than you have! "
+        << "Transaction is aborted.\n";
+    } else {
+        shares -= num;
+        share_val = price;
+        set_tot();
+    }
+}
+void Stock::update(double price) {
+    share_val = price;
+    set_tot();
+}
+void Stock::show() const {
+    using std::cout;
+    using std::ios_base;
+    // set format to #.###
+    ios_base::fmtflags orig =
+            cout.setf(ios_base::fixed, ios_base::floatfield);
+    std::streamsize prec = cout.precision(3);
+
+    cout << "Company: " << company
+    << " Shares: " << shares << '\n';
+    cout << " Share Price: $" << share_val;
+    // set format to #.##
+    cout.precision(2);
+    cout << " Total worth: $" << total_val << '\n';
+
+    // restore original format
+    cout.setf(orig, ios_base::floatfield);
+    cout.precision(prec);
+}
+
+const Stock & Stock::topval(const Stock &s) const {
+    if (s.total_val > total_val) {
+        return s;
+    } else {
+        return *this;
+    }
+}
+
+
+
+
+
+
+
